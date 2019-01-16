500091f190bb593858b4320b2b5a76a129859a47
Kian Taghavi <ktaghavi122091@gmail.com>
2019-01-16 13:03:24 -0600

    Update Detection.py

--- pelicannon-master/Detection.py
+++ pelicannon-master/Detection.py
@@ -10,7 +10,7 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 2, 6)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
